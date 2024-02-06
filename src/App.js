22148ee3e8612a3509c82738d1ba4ebd0077cf60
tahelena <44698097+tahelena@users.noreply.github.com>
2024-02-06 15:10:18 +1100

    list display

--- src/App.js
+++ src/App.js
@@ -1,5 +1,25 @@
- const App = () => {
-  return <div>Hey</div>;
+import { useState } from "react";
+import IconShow from "./components/IconShow";
+import './App.css';
+const getRandomIcon = () => {
+  const icons = ["cat", "sun", "dog", "cloud", "bell", "house"];
+  return icons[Math.floor(Math.random() * icons.length)];
+};
+
+const App = () => {
+  const [icons, setIcons] = useState([]);
+  const handleClick = () => {
+    setIcons([...icons, getRandomIcon()]);
+  };
+  const renderedIcons = icons.map((icon, index) => (
+    <IconShow type={icon} key={index} />
+  ));
+  return (
+    <div className="app">
+      <button onClick={handleClick}>Add icon</button>
+      <div className="icons-list">{renderedIcons}</div>
+    </div>
+  );
 };
 
 export default App;
