037bc248dd61c4ba1c428cc1c258454a0e104582
tahelena <44698097+tahelena@users.noreply.github.com>
2024-02-08 10:49:43 +1100

    end of section 10 - button

--- src/App.js
+++ src/App.js
@@ -1,5 +1,112 @@
- const App = () => {
-  return <div>Hey</div>;
+import Button from "./components/Button";
+
+const App = () => {
+  return (
+    <div className="flex flex-wrap gap-4 flex-col">
+      <div>
+        <span>
+          <Button primary onClick={() => console.log("Clicked")}>
+            Button Primary
+          </Button>
+        </span>
+        <span>
+          <Button secondary onMouseEnter={() => console.log("Mouse enter")}>
+            Button Secondary
+          </Button>
+        </span>
+        <span>
+          <Button danger>Button Success</Button>
+        </span>
+        <span>
+          <Button warning>Button Warning</Button>
+        </span>
+        <span>
+          <Button success>Button Danger</Button>
+        </span>
+      </div>
+      <div>
+        <span>
+          <Button primary rounded>
+            Button Primary
+          </Button>
+        </span>
+        <span>
+          <Button secondary rounded>
+            Button Secondary
+          </Button>
+        </span>
+        <span>
+          <Button danger rounded>
+            Button Success
+          </Button>
+        </span>
+        <span>
+          <Button warning rounded>
+            Button Warning
+          </Button>
+        </span>
+        <span>
+          <Button success rounded>
+            Button Danger
+          </Button>
+        </span>
+      </div>
+      <div>
+        <span>
+          <Button primary outline>
+            Button Primary
+          </Button>
+        </span>
+        <span>
+          <Button secondary outline>
+            Button Secondary
+          </Button>
+        </span>
+        <span>
+          <Button danger outline>
+            Button Success
+          </Button>
+        </span>
+        <span>
+          <Button warning outline>
+            Button Warning
+          </Button>
+        </span>
+        <span>
+          <Button success outline>
+            Button Danger
+          </Button>
+        </span>
+      </div>
+      <div>
+        <span>
+          <Button primary rounded outline>
+            Button Primary
+          </Button>
+        </span>
+        <span>
+          <Button secondary rounded outline>
+            Button Secondary
+          </Button>
+        </span>
+        <span>
+          <Button danger rounded outline>
+            Button Success
+          </Button>
+        </span>
+        <span>
+          <Button warning rounded outline>
+            Button Warning
+          </Button>
+        </span>
+        <span>
+          <Button success rounded outline>
+            Button Danger
+          </Button>
+        </span>
+      </div>
+    </div>
+  );
 };
 
 export default App;
---
+++ src/components/Button.js
@@ -0,0 +1,53 @@
+import classnames from "classnames";
+import { twMerge } from "tailwind-merge";
+
+const Button = ({
+  children,
+  primary,
+  secondary,
+  success,
+  warning,
+  danger,
+  outline,
+  rounded,
+  ...props
+}) => {
+  const classes = twMerge(
+    classnames(props.className, "px-3 py-1.5 border ", {
+      "border-blue-500 bg-blue-500 text-white": primary,
+      "border-blue-300 bg-blue-300 text-white": secondary,
+      "border-green-500 bg-green-500 text-white": success,
+      "border-orange-500 bg-orange-500 text-white": warning,
+      "border-red-500 bg-red-500 text-white": danger,
+      "rounded-full": rounded,
+      "bg-white": outline,
+      "text-blue-500": outline && primary,
+      "text-blue-300": outline && secondary,
+      "text-green-500": outline && success,
+      "text-orange-400": outline && warning,
+      "text-red-500": outline && danger,
+    })
+  );
+  return (
+    <button className={classes} {...props}>
+      {children}
+    </button>
+  );
+};
+
+Button.propTypes = {
+  checkType: ({ primary, secondary, success, warning, danger }) => {
+    const count =
+      Number(!!primary) +
+      Number(!!secondary) +
+      Number(!!success) +
+      Number(!!warning) +
+      Number(!!danger);
+    if (count > 1)
+      return new Error(
+        "Provided more than one button type, please revise your code."
+      );
+  },
+};
+
+export default Button;
