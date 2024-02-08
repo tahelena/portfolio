0c076603bc07052eaede003e8f6e832e197906ec
tahelena <44698097+tahelena@users.noreply.github.com>
2024-02-08 14:59:51 +1100

    last piece of section11

--- src/App.js
+++ src/App.js
@@ -1,28 +1,5 @@
-import Accordion from "./components/Accordion";
-
 const App = () => {
-  const items = [
-    {
-      label: "Label 1 ",
-      content:
-        "Content 1 Content 1 Content 1 Content 1 Content 1 Content 1 Content 1 Content 1 Content 1 Content 1 Content 1 Content 1 Content 1 Content 1 Content 1 Content 1 Content 1 Content 1 Content 1 Content 1 Content 1 Content 1 Content 1 Content 1 Content 1 Content 1 Content 1 Content 1 Content 1 Content 1 Content 1 Content 1 Content 1 Content 1 Content 1 Content 1 ",
-    },
-    {
-      label: "Label 2 ",
-      content:
-        "Content 2 Content 2 Content 2 Content 2 Content 2 Content 2 Content 2 Content 2 Content 2 Content 2 Content 2 Content 2 Content 2 Content 2 Content 2 Content 2 Content 2 Content 2 Content 2 Content 2 Content 2 Content 2 Content 2 Content 2 Content 2 Content 2 Content 2 Content 2 Content 2 Content 2 Content 2 Content 2 Content 2 Content 2 Content 2 Content 2 ",
-    },
-    {
-      label: "Label 3 ",
-      content:
-        "Content 3 Content 3 Content 3 Content 3 Content 3 Content 3 Content 3 Content 3 Content 3 Content 3 Content 3 Content 3 Content 3 Content 3 Content 3 Content 3 Content 3 Content 3 Content 3 Content 3 Content 3 Content 3 Content 3 Content 3 Content 3 Content 3 Content 3 Content 3 Content 3 Content 3 Content 3 Content 3 Content 3 Content 3 Content 3 Content 3 ",
-    },
-  ];
-  return (
-    <div>
-      <Accordion items={items} />
-    </div>
-  );
+  return <div></div>;
 };
 
 export default App;
---
+++ src/pages/AccordionPage.js
@@ -0,0 +1,28 @@
+import Accordion from "../components/Accordion";
+
+const AccordionPage = () => {
+  const items = [
+    {
+      label: "Label 1 ",
+      content:
+        "Content 1 Content 1 Content 1 Content 1 Content 1 Content 1 Content 1 Content 1 Content 1 Content 1 Content 1 Content 1 Content 1 Content 1 Content 1 Content 1 Content 1 Content 1 Content 1 Content 1 Content 1 Content 1 Content 1 Content 1 Content 1 Content 1 Content 1 Content 1 Content 1 Content 1 Content 1 Content 1 Content 1 Content 1 Content 1 Content 1 ",
+    },
+    {
+      label: "Label 2 ",
+      content:
+        "Content 2 Content 2 Content 2 Content 2 Content 2 Content 2 Content 2 Content 2 Content 2 Content 2 Content 2 Content 2 Content 2 Content 2 Content 2 Content 2 Content 2 Content 2 Content 2 Content 2 Content 2 Content 2 Content 2 Content 2 Content 2 Content 2 Content 2 Content 2 Content 2 Content 2 Content 2 Content 2 Content 2 Content 2 Content 2 Content 2 ",
+    },
+    {
+      label: "Label 3 ",
+      content:
+        "Content 3 Content 3 Content 3 Content 3 Content 3 Content 3 Content 3 Content 3 Content 3 Content 3 Content 3 Content 3 Content 3 Content 3 Content 3 Content 3 Content 3 Content 3 Content 3 Content 3 Content 3 Content 3 Content 3 Content 3 Content 3 Content 3 Content 3 Content 3 Content 3 Content 3 Content 3 Content 3 Content 3 Content 3 Content 3 Content 3 ",
+    },
+  ];
+  return (
+    <div>
+      <Accordion items={items} />
+    </div>
+  );
+};
+
+export default AccordionPage;
