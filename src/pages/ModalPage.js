53a500f023634f6d1033a264c4ffbc34aea82812
tahelena <44698097+tahelena@users.noreply.github.com>
2024-02-12 11:32:26 +1100

    complete section 14 - modal

--- src/pages/ModalPage.js
+++ src/pages/ModalPage.js
@@ -1,9 +1,32 @@
+import { useState } from "react";
+import Button from "../components/Button";
 import Modal from "../components/Modal";
 
 const ModalPage = () => {
+  const [showModal, setShowModal] = useState(false);
+
+  const handleClick = () => setShowModal(true);
+  const handleClose = () => setShowModal(false);
+
+  const actionBar = (
+    <div>
+      <Button primary rounded onClick={handleClose}>
+        Confirm
+      </Button>
+    </div>
+  );
+  const modalContent = <p>This is and modal description/text</p>;
+
   return (
     <div>
-      <Modal></Modal>
+      <Button primary rounded onClick={handleClick}>
+        Open modal
+      </Button>
+      {showModal && (
+        <Modal onClose={handleClose} actionBar={actionBar}>
+          {modalContent}
+        </Modal>
+      )}
     </div>
   );
 };
