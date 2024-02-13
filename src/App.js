e0e9531e1c876916d6e23f4d4ef8e4bfad7b23d7
tahelena <44698097+tahelena@users.noreply.github.com>
2024-02-13 11:21:34 +1100

    initial app setup

--- src/App.js
+++ src/App.js
@@ -1,5 +1,21 @@
- const App = () => {
-  return <div>Hey</div>;
-};
+import "./styles.css";
+import MoviePlaylist from "./components/MoviePlaylist";
+import SongPlaylist from "./components/SongPlaylist";
 
-export default App;
+export default function App() {
+  const handleResetClick = () => {
+    //
+  };
+
+  return (
+    <div className="container is-fluid">
+      <button onClick={() => handleResetClick()} className="button is-danger">
+        Reset Both Playlists
+      </button>
+      <hr />
+      <MoviePlaylist />
+      <hr />
+      <SongPlaylist />
+    </div>
+  );
+}
