55cce635b6ed7ed1bf5a64706bee405e24b92bf7
tahelena <44698097+tahelena@users.noreply.github.com>
2024-02-13 15:16:36 +1100

    started

---
+++ src/store/index.js
@@ -0,0 +1,19 @@
+import { configureStore, createSlice } from "@reduxjs/toolkit";
+
+const songsSlice = createSlice({
+  name: "song",
+  initialState: [],
+  reducers: {
+    addSong(state, action) {
+      state.push(action.payload);
+    },
+    removeSong(state, action) {},
+  },
+});
+
+const store = configureStore({
+  reducer: { songs: songsSlice.reducer },
+});
+
+const startingState = store.getState();
+console.log(startingState);
