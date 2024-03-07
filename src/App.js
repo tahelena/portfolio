4efdd939c88bb1abc0261ba172d22874d902a11c
tahelena <44698097+tahelena@users.noreply.github.com>
2024-03-07 11:36:50 +1100

    fix structure before merge

--- src/App.js
+++ src/App.js
@@ -1,24 +1,11 @@
-import { useDispatch } from "react-redux";
+
+import { MovieSongPage } from "./pages/MovieSongPage";
 import "./styles.css";
-import MoviePlaylist from "./components/MoviePlaylist";
-import SongPlaylist from "./components/SongPlaylist";
-import { reset } from "./store/index";
+
 
 export default function App() {
-  const dispatch = useDispatch();
-  const handleResetClick = () => {
-    dispatch(reset());
-  };
 
   return (
-    <div className="container is-fluid">
-      <button onClick={() => handleResetClick()} className="button is-danger">
-        Reset Both Playlists
-      </button>
-      <hr />
-      <MoviePlaylist />
-      <hr />
-      <SongPlaylist />
-    </div>
+  <MovieSongPage/>
   );
 }
