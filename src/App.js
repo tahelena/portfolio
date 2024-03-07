3d7d0d517f0304be70d83a1acbe22d1878fca7d5
tahelena <44698097+tahelena@users.noreply.github.com>
2024-03-07 16:28:56 +1100

    fix before merge

--- src/App.js
+++ src/App.js
@@ -1,24 +1,8 @@
-import { useState } from "react";
-import IconShow from "./components/IconShow";
-import './App.css';
-const getRandomIcon = () => {
-  const icons = ["cat", "sun", "dog", "cloud", "bell", "house"];
-  return icons[Math.floor(Math.random() * icons.length)];
-};
+import RandomIconsPage from "./pages/RandomIconsPage";
 
 const App = () => {
-  const [icons, setIcons] = useState([]);
-  const handleClick = () => {
-    setIcons([...icons, getRandomIcon()]);
-  };
-  const renderedIcons = icons.map((icon, index) => (
-    <IconShow type={icon} key={index} />
-  ));
   return (
-    <div className="app">
-      <button onClick={handleClick}>Add icon</button>
-      <div className="icons-list">{renderedIcons}</div>
-    </div>
+     <RandomIconsPage/>
   );
 };
 
---
+++ src/pages/RandomIconsPage.js
@@ -0,0 +1,26 @@
+import { useState } from "react";
+import IconShow from "../components/IconShow";
+import "../styles/IconShow.css";
+
+const getRandomIcon = () => {
+  const icons = ["cat", "sun", "dog", "cloud", "bell", "house"];
+  return icons[Math.floor(Math.random() * icons.length)];
+};
+
+const RandomIconsPage = () => {
+  const [icons, setIcons] = useState([]);
+  const handleClick = () => {
+    setIcons([...icons, getRandomIcon()]);
+  };
+  const renderedIcons = icons.map((icon, index) => (
+    <IconShow type={icon} key={index} />
+  ));
+  return (
+    <div className="app">
+      <button onClick={handleClick}>Add icon</button>
+      <div className="icons-list">{renderedIcons}</div>
+    </div>
+  );
+};
+
+export default RandomIconsPage;
